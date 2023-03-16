6bde83a9d687594d4ed260fbfb4f99ba3ed415a8
FosEd <114414542+FosEd@users.noreply.github.com>
2023-03-16 16:12:01 +0000

    Add files via upload

---
+++ final_project/Final_Project/src/final_project/ProfileList.java
@@ -0,0 +1,25 @@
+package final_project;
+import java.util.LinkedList;
+
+public class ProfileList {
+	private LinkedList<Users> friends = new LinkedList<Users>();
+	
+	public ProfileList() {
+		
+	}
+	
+	public void addFriend(Users friend) {
+		friends.add(friend);
+		
+	}
+	
+	public Users findFriend() {
+		
+	}
+	
+	public boolean removeFriend() {
+		
+	}
+	
+	public 
+}
