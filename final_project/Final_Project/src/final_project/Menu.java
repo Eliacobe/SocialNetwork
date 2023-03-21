44b7aa0098a89e9a4de8032dad0ede24863e65e9
Eliacobe <114414469+Eliacobe@users.noreply.github.com>
2023-03-21 13:10:02 +0000

    Start menu

--- final_project/Final_Project/src/final_project/Menu.java
+++ final_project/Final_Project/src/final_project/Menu.java
@@ -1,1 +1,28 @@
+package final_project;
+import java.util.*;
+import java.io.*;
 
+
+public class Menu
+{
+
+    public void profile()
+    {
+        System.out.println(User.getName() + "             " + User.getUserID());
+    }
+
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("To view your profile press 1: ");
+        System.out.println("To view your friends list press 2: ");
+        System.out.println("To view your posts press 3: ");
+        int ans = scan.nextInt();
+
+        if(ans == 1)
+        {
+            profile();
+        }
+    }
+}
