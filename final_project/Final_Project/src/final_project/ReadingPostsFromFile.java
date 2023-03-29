2ceb7e03baa91071432c932c28548970a7322969
Eliacobe <114414469+Eliacobe@users.noreply.github.com>
2023-03-29 17:13:46 +0100

    flushed screen for looking better

--- final_project/Final_Project/src/final_project/ReadingPostsFromFile.java
+++ final_project/Final_Project/src/final_project/ReadingPostsFromFile.java
@@ -11,19 +11,24 @@ public class ReadingPostsFromFile
         this.posts = new ArrayList<>();
     }
 
-    public void readFromFile(String filename) throws IOException
+    public void readFromFile(String filename) throws FileNotFoundException, IOException
     {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename)))
+        String file = filename + ".txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(file)))
         {
             String line;
-            while ((line = reader.readLine()) != null)
-            {
-                String[] fields = line.split(",");
-                String postID = fields[0];
-                String content = fields[1];
-                int likes = Integer.parseInt(fields[2]);
-                UserPost post = new UserPost(postID, filename, content, likes);
-                posts.add(post); 
+            try {
+                while ((line = reader.readLine()) != null)
+                {
+                    String[] fields = line.split(",");
+                    String postID = fields[0];
+                    String content = fields[1];
+                    int likes = Integer.parseInt(fields[2]);
+                    UserPost post = new UserPost(postID, filename, content, likes);
+                    posts.add(post); 
+                }
+            } catch (NumberFormatException e) {
+            } catch (IOException e) {
             }
         }
     }
