4916ae85a396081912432ef3ec915daca686778a
L0lip <114414422+L0lip@users.noreply.github.com>
2023-03-29 20:31:12 +0100

    commenting and txt fixes

--- final_project/Final_Project/src/final_project/User.java
+++ final_project/Final_Project/src/final_project/User.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class User
 {
+    // Creates variable for the user
     private String userID;
     private String name;
     private String workplace;
@@ -12,6 +13,7 @@ public class User
     
     public User(String userID, String name, String workplace, String hometown)
     {
+        // Constructor method for User
         this.userID = userID;
         this.name = name;
         this.workplace = workplace;
